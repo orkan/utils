20a3a926536f9a005d319cc80874a5aca5afe6b6
Orkan <orkans@gmail.com>
2021-12-07 06:26:23 +0100

    [Tools][cryptobox] Code clearing

--- tools/cryptobox/rename_dirs.bat
+++ tools/cryptobox/rename_dirs.bat
@@ -21,10 +21,10 @@ echo.
 REM Import: --------------------------------------------
 REM For shortcuts (*.lnk) the dropped file path is always added at the end (by Windows)
 REM So <DVR dir> must be the last param
-set DVRDIR=%~1
+set HOMEDIR=%~1
 
 REM Delegate job to PHP...
-php %~n0.php "%DVRDIR%"
+php %~n0.php "%HOMEDIR%"
 
 REM Show BUILD status
 call ..\bat\exit_status.bat
