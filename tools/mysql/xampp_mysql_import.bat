95a25df9c64df1b6f7c03ac7dc1b397cd1aae0ff
Orkan <orkans@gmail.com>
2022-07-03 16:03:25 +0200

    [Tools][mysql] Update import script

--- tools/mysql/xampp_mysql_import.bat
+++ tools/mysql/xampp_mysql_import.bat
@@ -1,32 +1,50 @@
 @echo off
-echo %~0
 setlocal
 
 echo ***************************************************
 echo   XAMPP MySQL import script
-echo   Usage: %~nx0 username database "path to sql file"
+echo   Usage: %~f0 ^<database^> ^<sql file^>
 echo ***************************************************
-
-REM Config: -------------------------------------------------
-set MYSQL_HOME=C:\mysql\xampp\mysql
-set MYSQL_USER=%1
-set MYSQL_DB=%2
-set MYSQL_FILE=%~3
 echo.
 
-REM Commands: -------------------------------------------------
-set  COMMAND= %MYSQL_HOME%\bin\mysql.exe -u %MYSQL_USER% -p %MYSQL_DB% ^< "%MYSQL_FILE%"
-echo COMMAND: %MYSQL_HOME%\bin\mysql.exe -u %MYSQL_USER% -p %MYSQL_DB% ^< "%MYSQL_FILE%"
-REM echo %COMMAND%
-REM Can't echo %COMMAND% ??? "The syntax of the command is incorrect."
-REM -----------------------------------------------------------
+REM Import: -------------------------------------------
+set MYSQL_DB=%1
+set MYSQL_FILE=%2
 
-if "%MYSQL_USER%" == "" goto error
+REM Config: -------------------------------------------------
+set MYSQL_PATH=C:\mysql\xampp\mysql\bin\mysql.exe
+set MYSQL_HOST=3307
+set MYSQL_PORT=3307
+set MYSQL_USER=root
+set COMMAND=%MYSQL_PATH% -h 127.0.0.1 --port=3307 -u root %MYSQL_DB%
+
+REM Display: ------------------------------------------
+echo Inputs:
+echo database : [%MYSQL_DB%]
+echo sql file : [%MYSQL_FILE%]
+echo.
+
+echo Command:
+echo %COMMAND% ^< %MYSQL_FILE%
+echo.
+
 if "%MYSQL_DB%" == "" goto error
 if "%MYSQL_FILE%" == "" goto error
 if not exist %MYSQL_FILE% goto error
+pause
 
-cmd /k %COMMAND%
+REM Run: ------------------------------------------------------
+%COMMAND% < %MYSQL_FILE%
+
+if %ERRORLEVEL% NEQ 0 (
+	echo BUILD FAILED
+)
+
+goto :end
 
 :error
 echo Wrong parameters
+
+:end
+echo.
+pause
