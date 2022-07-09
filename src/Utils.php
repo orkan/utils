73bac81afb82a4b1dc1d4e136b1ddee2455db2f4
Orkan <orkans@gmail.com>
2022-07-09 14:28:03 +0200

    [Utils] Remove JSON/CURL constants

--- src/Utils.php
+++ src/Utils.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * This file is part of the orkan/utils package.
+ * Copyright (c) 2020-@Year@ Orkan <orkans@gmail.com>
+ */
 namespace Orkan;
 
 /**
@@ -8,92 +12,6 @@ namespace Orkan;
  */
 class Utils
 {
-	/* @formatter:off */
-	const JSON_ERROR = [
-		JSON_ERROR_DEPTH				 => ['JSON_ERROR_DEPTH',				 'The maximum stack depth has been exceeded'],
-		JSON_ERROR_STATE_MISMATCH		 => ['JSON_ERROR_STATE_MISMATCH',		 'Invalid or malformed JSON'],
-		JSON_ERROR_CTRL_CHAR			 => ['JSON_ERROR_CTRL_CHAR',			 'Control character error, possibly incorrectly encoded'],
-		JSON_ERROR_SYNTAX				 => ['JSON_ERROR_SYNTAX',				 'Syntax error'],
-		JSON_ERROR_UTF8					 => ['JSON_ERROR_UTF8',					 'Malformed UTF-8 characters, possibly incorrectly encoded'],
-		JSON_ERROR_RECURSION			 => ['JSON_ERROR_RECURSION',			 'One or more recursive references in the value to be encoded'],
-		JSON_ERROR_INF_OR_NAN			 => ['JSON_ERROR_INF_OR_NAN',			 'One or more NAN or INF values in the value to be encoded'],
-		JSON_ERROR_UNSUPPORTED_TYPE		 => ['JSON_ERROR_UNSUPPORTED_TYPE',		 'A value of a type that cannot be encoded was given'],
-		JSON_ERROR_INVALID_PROPERTY_NAME => ['JSON_ERROR_INVALID_PROPERTY_NAME', 'A property name that cannot be encoded was given'],
-		JSON_ERROR_UTF16				 => ['JSON_ERROR_UTF16',				 'Malformed UTF-16 characters, possibly incorrectly encoded'],
-	];
-
-	const CURLE_ERROR = [
-		CURLE_UNSUPPORTED_PROTOCOL        => ['CURLE_UNSUPPORTED_PROTOCOL',        ''],
-		CURLE_FAILED_INIT                 => ['CURLE_FAILED_INIT',                 ''],
-		CURLE_URL_MALFORMAT               => ['CURLE_URL_MALFORMAT',               ''],
-		CURLE_URL_MALFORMAT_USER          => ['CURLE_URL_MALFORMAT_USER',          ''],
-		CURLE_COULDNT_RESOLVE_PROXY       => ['CURLE_COULDNT_RESOLVE_PROXY',       ''],
-		CURLE_COULDNT_RESOLVE_HOST        => ['CURLE_COULDNT_RESOLVE_HOST',        ''],
-		CURLE_COULDNT_CONNECT             => ['CURLE_COULDNT_CONNECT',             ''],
-		CURLE_FTP_WEIRD_SERVER_REPLY      => ['CURLE_FTP_WEIRD_SERVER_REPLY',      ''],
-		CURLE_FTP_ACCESS_DENIED           => ['CURLE_FTP_ACCESS_DENIED',           ''],
-		CURLE_FTP_USER_PASSWORD_INCORRECT => ['CURLE_FTP_USER_PASSWORD_INCORRECT', ''],
-		CURLE_FTP_WEIRD_PASS_REPLY        => ['CURLE_FTP_WEIRD_PASS_REPLY',        ''],
-		CURLE_FTP_WEIRD_USER_REPLY        => ['CURLE_FTP_WEIRD_USER_REPLY',        ''],
-		CURLE_FTP_WEIRD_PASV_REPLY        => ['CURLE_FTP_WEIRD_PASV_REPLY',        ''],
-		CURLE_FTP_WEIRD_227_FORMAT        => ['CURLE_FTP_WEIRD_227_FORMAT',        ''],
-		CURLE_FTP_CANT_GET_HOST           => ['CURLE_FTP_CANT_GET_HOST',           ''],
-		CURLE_FTP_CANT_RECONNECT          => ['CURLE_FTP_CANT_RECONNECT',          ''],
-		CURLE_FTP_COULDNT_SET_BINARY      => ['CURLE_FTP_COULDNT_SET_BINARY',      ''],
-		CURLE_FTP_PARTIAL_FILE            => ['CURLE_FTP_PARTIAL_FILE',            ''],
-		CURLE_PARTIAL_FILE                => ['CURLE_PARTIAL_FILE',                ''],
-		CURLE_FTP_COULDNT_RETR_FILE       => ['CURLE_FTP_COULDNT_RETR_FILE',       ''],
-		CURLE_FTP_WRITE_ERROR             => ['CURLE_FTP_WRITE_ERROR',             ''],
-		CURLE_FTP_QUOTE_ERROR             => ['CURLE_FTP_QUOTE_ERROR',             ''],
-		CURLE_HTTP_NOT_FOUND              => ['CURLE_HTTP_NOT_FOUND',              ''],
-		CURLE_HTTP_RETURNED_ERROR         => ['CURLE_HTTP_RETURNED_ERROR',         ''],
-		CURLE_WRITE_ERROR                 => ['CURLE_WRITE_ERROR',                 ''],
-		CURLE_MALFORMAT_USER              => ['CURLE_MALFORMAT_USER',              ''],
-		CURLE_FTP_COULDNT_STOR_FILE       => ['CURLE_FTP_COULDNT_STOR_FILE',       ''],
-		CURLE_READ_ERROR                  => ['CURLE_READ_ERROR',                  ''],
-		CURLE_OUT_OF_MEMORY               => ['CURLE_OUT_OF_MEMORY',               ''],
-		CURLE_OPERATION_TIMEOUTED         => ['CURLE_OPERATION_TIMEOUTED',         ''],
-		CURLE_OPERATION_TIMEDOUT          => ['CURLE_OPERATION_TIMEDOUT',          ''],
-		CURLE_FTP_COULDNT_SET_ASCII       => ['CURLE_FTP_COULDNT_SET_ASCII',       ''],
-		CURLE_FTP_PORT_FAILED             => ['CURLE_FTP_PORT_FAILED',             ''],
-		CURLE_FTP_COULDNT_USE_REST        => ['CURLE_FTP_COULDNT_USE_REST',        ''],
-		CURLE_FTP_COULDNT_GET_SIZE        => ['CURLE_FTP_COULDNT_GET_SIZE',        ''],
-		CURLE_HTTP_RANGE_ERROR            => ['CURLE_HTTP_RANGE_ERROR',            ''],
-		CURLE_HTTP_POST_ERROR             => ['CURLE_HTTP_POST_ERROR',             ''],
-		CURLE_SSL_CONNECT_ERROR           => ['CURLE_SSL_CONNECT_ERROR',           ''],
-		CURLE_FTP_BAD_DOWNLOAD_RESUME     => ['CURLE_FTP_BAD_DOWNLOAD_RESUME',     ''],
-		CURLE_BAD_DOWNLOAD_RESUME         => ['CURLE_BAD_DOWNLOAD_RESUME',         ''],
-		CURLE_FILE_COULDNT_READ_FILE      => ['CURLE_FILE_COULDNT_READ_FILE',      ''],
-		CURLE_LDAP_CANNOT_BIND            => ['CURLE_LDAP_CANNOT_BIND',            ''],
-		CURLE_LDAP_SEARCH_FAILED          => ['CURLE_LDAP_SEARCH_FAILED',          ''],
-		CURLE_LIBRARY_NOT_FOUND           => ['CURLE_LIBRARY_NOT_FOUND',           ''],
-		CURLE_FUNCTION_NOT_FOUND          => ['CURLE_FUNCTION_NOT_FOUND',          ''],
-		CURLE_ABORTED_BY_CALLBACK         => ['CURLE_ABORTED_BY_CALLBACK',         ''],
-		CURLE_BAD_FUNCTION_ARGUMENT       => ['CURLE_BAD_FUNCTION_ARGUMENT',       ''],
-		CURLE_BAD_CALLING_ORDER           => ['CURLE_BAD_CALLING_ORDER',           ''],
-		CURLE_HTTP_PORT_FAILED            => ['CURLE_HTTP_PORT_FAILED',            ''],
-		CURLE_BAD_PASSWORD_ENTERED        => ['CURLE_BAD_PASSWORD_ENTERED',        ''],
-		CURLE_TOO_MANY_REDIRECTS          => ['CURLE_TOO_MANY_REDIRECTS',          ''],
-		CURLE_UNKNOWN_TELNET_OPTION       => ['CURLE_UNKNOWN_TELNET_OPTION',       ''],
-		CURLE_TELNET_OPTION_SYNTAX        => ['CURLE_TELNET_OPTION_SYNTAX',        ''],
-		CURLE_OBSOLETE                    => ['CURLE_OBSOLETE',                    ''],
-		CURLE_SSL_PEER_CERTIFICATE        => ['CURLE_SSL_PEER_CERTIFICATE',        ''],
-		CURLE_GOT_NOTHING                 => ['CURLE_GOT_NOTHING',                 ''],
-		CURLE_SSL_ENGINE_NOTFOUND         => ['CURLE_SSL_ENGINE_NOTFOUND',         ''],
-		CURLE_SSL_ENGINE_SETFAILED        => ['CURLE_SSL_ENGINE_SETFAILED',        ''],
-		CURLE_SEND_ERROR                  => ['CURLE_SEND_ERROR',                  ''],
-		CURLE_RECV_ERROR                  => ['CURLE_RECV_ERROR',                  ''],
-		CURLE_SHARE_IN_USE                => ['CURLE_SHARE_IN_USE',                ''],
-		CURLE_SSL_CERTPROBLEM             => ['CURLE_SSL_CERTPROBLEM',             ''],
-		CURLE_SSL_CIPHER                  => ['CURLE_SSL_CIPHER',                  ''],
-		CURLE_SSL_CACERT                  => ['CURLE_SSL_CACERT',                  ''],
-		CURLE_BAD_CONTENT_ENCODING        => ['CURLE_BAD_CONTENT_ENCODING',        ''],
-		CURLE_LDAP_INVALID_URL            => ['CURLE_LDAP_INVALID_URL',            ''],
-		CURLE_FILESIZE_EXCEEDED           => ['CURLE_FILESIZE_EXCEEDED',           ''],
-		CURLE_FTP_SSL_FAILED              => ['CURLE_FTP_SSL_FAILED',              ''],
-		CURLE_SSH                         => ['CURLE_SSH',                         ''],
-	];
-	/* @formatter:on */
 
 	/**
 	 * Format byte size string
@@ -424,11 +342,26 @@ class Utils
 	 */
 	public static function getJsonLastError(): string
 	{
+		/* @formatter:off */
+		static $jsonError = [
+			JSON_ERROR_DEPTH				 => ['JSON_ERROR_DEPTH',				 'The maximum stack depth has been exceeded'],
+			JSON_ERROR_STATE_MISMATCH		 => ['JSON_ERROR_STATE_MISMATCH',		 'Invalid or malformed JSON'],
+			JSON_ERROR_CTRL_CHAR			 => ['JSON_ERROR_CTRL_CHAR',			 'Control character error, possibly incorrectly encoded'],
+			JSON_ERROR_SYNTAX				 => ['JSON_ERROR_SYNTAX',				 'Syntax error'],
+			JSON_ERROR_UTF8					 => ['JSON_ERROR_UTF8',					 'Malformed UTF-8 characters, possibly incorrectly encoded'],
+			JSON_ERROR_RECURSION			 => ['JSON_ERROR_RECURSION',			 'One or more recursive references in the value to be encoded'],
+			JSON_ERROR_INF_OR_NAN			 => ['JSON_ERROR_INF_OR_NAN',			 'One or more NAN or INF values in the value to be encoded'],
+			JSON_ERROR_UNSUPPORTED_TYPE		 => ['JSON_ERROR_UNSUPPORTED_TYPE',		 'A value of a type that cannot be encoded was given'],
+			JSON_ERROR_INVALID_PROPERTY_NAME => ['JSON_ERROR_INVALID_PROPERTY_NAME', 'A property name that cannot be encoded was given'],
+			JSON_ERROR_UTF16				 => ['JSON_ERROR_UTF16',				 'Malformed UTF-16 characters, possibly incorrectly encoded'],
+		];
+		/* @formatter:on */
+
 		$result = '';
 
 		if ( JSON_ERROR_NONE != $lastError = json_last_error() ) {
-			$key = self::JSON_ERROR[$lastError][0] ?? '???';
-			$msg = self::JSON_ERROR[$lastError][1] ?? 'Definition missing for error #' . $lastError;
+			$key = $jsonError[$lastError][0] ?? '???';
+			$msg = $jsonError[$lastError][1] ?? 'Definition missing for error #' . $lastError;
 			$result = "$key: $msg";
 		}
 
