522e4fc16badba3a36d53eedd1d3e98227c3358f
Orkan <orkans@gmail.com>
2022-08-29 17:28:19 +0200

    [PHPUnit][Coverage] Update to new xdebug

--- tools/phpunit/coverage.bat
+++ tools/phpunit/coverage.bat
@@ -1,15 +1,20 @@
 @echo off
 call %~dp0_header.bat "%~f0"
 
+REM Setup xdebug: ---------------------------------------------
+set XDEBUG_MODE=coverage
+
 setlocal
 set VENDOR=%~1
 set EXTRAS=%~2
 set OUTDIR=%~3
 
 REM Clear coverag dir: ----------------------------------------
-rd /s /q %OUTDIR%
+if exist "%OUTDIR%" (
+	rd /s /q "%OUTDIR%"
+)
 
 REM Config: ---------------------------------------------------
-set OPTIONS=--coverage-html %OUTDIR%
+set OPTIONS=--coverage-html "%OUTDIR%"
 
 call %~dp0_runner.bat "%VENDOR%" "%EXTRAS%" "%OPTIONS%"
