ddb165e174485fd7e1a98f09d7a8da87300ec70c
Orkan <orkans@gmail.com>
2022-10-08 14:40:04 +0200

    [PHPUnit/_cmd] Validate _config paths

--- tests/_cmd/_config.bat
+++ tests/_cmd/_config.bat
@@ -5,5 +5,14 @@ set DEBUG=
 
 REM These paths are correct for location:
 REM vendor\[vendor_name]\[package_name]\tests\_cmd\_config.bat
-set RUNNER_DIR=%~dp0..\..\..\utils\tools\phpunit
 set VENDOR_DIR=%~dp0..\..\..\..\..\vendor
+set RUNNER_DIR=%VENDOR_DIR%\orkan\utils\tools\phpunit
+
+if not exist "%VENDOR_DIR%" (
+	echo [%~nx0] VENDOR_DIR not found! Using: "%VENDOR_DIR%"
+	exit /b 404
+)
+if not exist "%RUNNER_DIR%" (
+	echo [%~nx0] RUNNER_DIR not found! Using: "%RUNNER_DIR%"
+	exit /b 404
+)
